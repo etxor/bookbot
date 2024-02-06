80c58e1815f614f5797b2f6d915986bfe00c2b49
etxor <159087894+etxor@users.noreply.github.com>
2024-02-05 19:36:12 -0600

    Get chars dict

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankestein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
-    print(num_words)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path) as file:
@@ -11,5 +12,17 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for char in text:
+        if not char.isalpha():
+            continue
+        lower = char.lower()
+        if lower in chars:
+            chars[lower] += 1
+        else:
+            chars[lower] = 1
+    return chars
 
 main()
